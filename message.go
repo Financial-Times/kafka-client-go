43e4297edecf76c5509e883346ebf8ba6d4379fc
atanasdinov <atanas.dinov@ft.com>
2022-10-28 16:53:17 +0300

    Add message and headers definitions

---
+++ message.go
@@ -0,0 +1,51 @@
+package kafka
+
+import (
+	"github.com/Shopify/sarama"
+)
+
+const (
+	SchemaNameHeader    = "X-Schema-Name"
+	SchemaVersionHeader = "X-Schema-Version"
+)
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return map[string]string{}
+}
+
+func (h Headers) toRecordHeaders() []sarama.RecordHeader {
+	var headers []sarama.RecordHeader
+
+	for key, value := range h {
+		headers = append(headers, sarama.RecordHeader{
+			Key:   []byte(key),
+			Value: []byte(value),
+		})
+	}
+
+	return headers
+}
+
+type Message struct {
+	Key     string
+	Value   []byte
+	Headers Headers
+}
+
+func rawToMessage(message *sarama.ConsumerMessage) *Message {
+	headers := map[string]string{}
+	for _, header := range message.Headers {
+		key := string(header.Key)
+		value := string(header.Value)
+
+		headers[key] = headers[value]
+	}
+
+	return &Message{
+		Key:     string(message.Key),
+		Value:   message.Value,
+		Headers: headers,
+	}
+}
